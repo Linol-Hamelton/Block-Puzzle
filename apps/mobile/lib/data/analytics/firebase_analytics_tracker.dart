de88eb062bf92d4f4aae9b6b11d9b20fb7fbacdd
Linol-Hamelton <Linol-Hamelton@users.noreply.github.com>
2026-04-27 21:13:33 +0300

    fix(ci): type cast Map values for Firebase Analytics

--- apps/mobile/lib/data/analytics/firebase_analytics_tracker.dart
+++ apps/mobile/lib/data/analytics/firebase_analytics_tracker.dart
@@ -13,9 +13,19 @@ class FirebaseAnalyticsTracker implements AnalyticsTracker {
     String eventName, {
     Map<String, Object?> params = const <String, Object?>{},
   }) async {
+    Map<String, Object>? safeParams;
+    if (params.isNotEmpty) {
+      safeParams = <String, Object>{};
+      for (final MapEntry<String, Object?> entry in params.entries) {
+        if (entry.value != null) {
+          safeParams[entry.key] = entry.value!;
+        }
+      }
+    }
+
     await _analytics.logEvent(
       name: eventName,
-      parameters: params,
+      parameters: safeParams,
     );
   }
 
